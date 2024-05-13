0e028ffdb4637f22482936760bcbfddb057ea81e
NandhuVsona <naveenvsona@gmail.com>
2024-05-13 05:47:18 +0530

    first commit

---
+++ public/100data.js
@@ -0,0 +1,244 @@
+let results = [
+  {
+    name: {
+      title: "Ms",
+      first: "Sharmila",
+      last: "Devi",
+    },
+    destination: "Assistant professor",
+    location: {
+      street: {},
+    },
+    email: "sharmiladevi@sonatech.ac.in",
+    login: {
+      uuid: "41fcf11c-872d-4c13-9976-f4e9c9992aaf",
+      username: "happywolf377",
+      password: "jumbo",
+    },
+    picture: {
+      medium: "images/sharmila.png",
+    },
+  },
+  {
+    name: {
+      title: "Ms",
+      first: "Jamuna",
+      last: "Rani",
+    },
+    destination: "Associate professor",
+    location: {
+      street: {},
+    },
+    email: "jamunarani.m@sonatech.ac.in",
+    login: {
+      uuid: "42fcf11c-872d-4c13-9976-f4e9c9992aaf",
+      username: "happywolf377",
+      password: "jumbo",
+    },
+    picture: {
+      medium: "images/jamuna.png",
+    },
+  },
+  {
+    name: {
+      title: "Mr",
+      first: "Paramasivam",
+      last: "M",
+    },
+    destination: "Assistant professor",
+    location: {
+      street: {},
+    },
+    email: "sivam@sonatech.ac.in",
+    login: {
+      uuid: "42fcf11c-872d-4c13-9976-f4e9c9902aaf",
+      username: "happywolf377",
+      password: "jumbo",
+    },
+    picture: {
+      medium: "images/parmashivam.png",
+    },
+  },
+  {
+    name: {
+      title: "Mr",
+      first: "Neel ",
+      last: "Kamal Shajahan",
+    },
+    destination: "Assistant professor",
+    location: {
+      street: {},
+    },
+    email: "neelkamal@sonatech.ac.in",
+    login: {
+      uuid: "42fcf11c-872d-4c13-9976-f4e9c9992aag",
+      username: "happywolf377",
+      password: "jumbo",
+    },
+    picture: {
+      medium: "images/neelkamal.png",
+    },
+  },
+  {
+    name: {
+      title: "Mr",
+      first: "Prasad",
+      last: "B",
+    },
+    destination: "Assistant professor",
+    location: {
+      street: {},
+    },
+    email: "neelkamal@sonatech.ac.in",
+    login: {
+      uuid: "42fcf11c-872d-4c13-9976-f4e9c9992aae",
+      username: "happywolf377",
+      password: "jumbo",
+    },
+    picture: {
+      medium: "images/prasad.png",
+    },
+  },
+  {
+    name: {
+      title: "Mr",
+      first: "Hema",
+      last: " Kumar M",
+    },
+    destination: "Assistant professor",
+    location: {
+      street: {},
+    },
+    email: "hemakumar.ece@sonatech.ac.in",
+    login: {
+      uuid: "42fcf11c-872d-4c13-9976-f4e9c9992aaz",
+      username: "happywolf377",
+      password: "jumbo",
+    },
+    picture: {
+      medium: "images/hemakumar.png",
+    },
+  },
+  {
+    name: {
+      title: "Ms",
+      first: "Vijayalakshmi",
+      last: "S",
+    },
+    destination: "Assistant professor",
+    location: {
+      street: {},
+    },
+    email: "vijayalakshmis@sonatech.ac.in",
+    login: {
+      uuid: "42fcf11c-872d-4c13-9976-f4e9c9992awf",
+      username: "happywolf377",
+      password: "jumbo",
+    },
+    picture: {
+      medium: "images/vijaylakshmi.png",
+    },
+  },
+  {
+    name: {
+      title: "Ms",
+      first: "Senthil",
+      last: " Vadivu M",
+    },
+    destination: "Assistant professor",
+    location: {
+      street: {},
+    },
+    email: "senthilvadivu@sonatech.ac.in",
+    login: {
+      uuid: "42fcf11c-872d-4c13-9976-f4e9c9992raf",
+      username: "happywolf377",
+      password: "jumbo",
+    },
+    picture: {
+      medium: "images/senthilv.png",
+    },
+  },
+  {
+    name: {
+      title: "Mr",
+      first: "Suriyakrishnaan",
+      last: "K",
+    },
+    destination: "Assistant professor",
+    location: {
+      street: {},
+    },
+    email: "suriyakrishnaan.ece@sonatech.ac.in",
+    login: {
+      uuid: "42fcf11c-872d-4c13-9976-f4e9c3992aaf",
+      username: "happywolf377",
+      password: "jumbo",
+    },
+    picture: {
+      medium: "images/suriyakrishna.png",
+    },
+  },
+  {
+    name: {
+      title: "Ms",
+      first: "Saranya",
+      last: "K",
+    },
+    destination: "Assistant professor",
+    location: {
+      street: {},
+    },
+    email: "saranya.k@sonatech.ac.in",
+    login: {
+      uuid: "42fcf11c-872d-4c13-9976-f4e9c4992aaf",
+      username: "happywolf377",
+      password: "jumbo",
+    },
+    picture: {
+      medium: "images/saranya.png",
+    },
+  },
+  {
+    name: {
+      title: "Ms",
+      first: "Vijayashaarathi",
+      last: "S",
+    },
+    destination: "Assistant professor",
+    location: {
+      street: {},
+    },
+    email: "vijayashaarathi.s@sonatech.ac.in",
+    login: {
+      uuid: "42fcf11c-872d-4c13-9986-f4e9c9992aaf",
+      username: "happywolf377",
+      password: "jumbo",
+    },
+    picture: {
+      medium: "images/vijayasatathi.png",
+    },
+  },
+  {
+    name: {
+      title: "Ms",
+      first: "Sasirekha",
+      last: "N",
+    },
+    destination: "Assistant professor",
+    location: {
+      street: {},
+    },
+    email: "sasirekha.n@sonatech.ac.in",
+    login: {
+      uuid: "43fcf11c-872d-4c13-9976-f4e9c9902aaf",
+      username: "happywolf377",
+      password: "jumbo",
+    },
+    picture: {
+      medium: "images/sasireka.png",
+    },
+  },
+  
+];
+export default results;
